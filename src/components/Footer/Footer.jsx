d98b203bbb30b31bd3a7b52f1e56c89d786503a8
AlexandreAkkt <alexandreskkt@gmail.com>
2026-07-21 20:33:38 -0300

    feat: adiciona componente Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,17 @@
-function Footer(){
-    return<h1> Sou um Footer</h1>
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <h2>🧠 NeuroPsico</h2>
+
+      <p>Neuropsicopedagogia com afeto e ciência</p>
+
+      <span>
+        © 2026 NeuroPsico. Todos os direitos reservados.
+      </span>
+    </footer>
+  );
 }
+
 export default Footer;
