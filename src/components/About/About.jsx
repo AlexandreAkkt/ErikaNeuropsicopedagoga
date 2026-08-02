4bd9706bc9735f4995f6a9c5553939375a6a294b
AlexandreAkkt <alexandreskkt@gmail.com>
2026-08-01 21:15:18 -0300

    Criando About.jsx e css

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,43 @@
-function About(){
-    return<h1> Sou um About</h1>
+import "./About.css";
+import erikaAbout from "../../assets/erika.png";
+
+function About() {
+  return (
+    <section id="about">
+      <div className="container about">
+
+        <div className="about-image">
+          <img src={erikaAbout} alt="Neuropsicopedagoga Erika" />
+        </div>
+
+        <div className="about-content">
+
+          <span className="about-tag">
+            Sobre
+          </span>
+
+          <h2>
+            Cuidando do desenvolvimento infantil com acolhimento e dedicação.</h2>
+
+          <p>
+            Sou Neuropsicopedagoga e acredito que cada criança possui um jeito
+            único de aprender. Meu trabalho é compreender suas necessidades e
+            desenvolver estratégias que favoreçam seu desenvolvimento cognitivo,
+            emocional e escolar.
+          </p>
+
+          <ul>
+            <li>✔ Atendimento individualizado</li>
+            <li>✔ Avaliação Neuropsicopedagógica</li>
+            <li>✔ Intervenção nas dificuldades de aprendizagem</li>
+            <li>✔ Orientação para famílias</li>
+          </ul>
+
+        </div>
+
+      </div>
+    </section>
+  );
 }
+
 export default About;
