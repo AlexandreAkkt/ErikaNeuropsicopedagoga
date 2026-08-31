bc6696c5902e4cf3a495b7deb55f07b1def96dd4
Alexandre Henrique dos Anjos <alexandreskkt@gmail.com>
2026-08-31 16:18:55 -0300

    atualiza imagem do hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,30 +1,25 @@
 import "./Hero.css";
-import erika from "../../assets/logoNovo.png";
+import erika from "../../assets/neuro.png";
 
 function Hero() {
   return (
     <section className="hero">
       <div className="hero-content">
         <span className="hero-tag">
-           Cuidado especializado para seu desenvolvimento
+          Cuidado especializado para seu desenvolvimento
         </span>
 
         <h1>
-          Neuropsicopedagogia com  afeto e ciência. <br />
-          
+          Neuropsicopedagogia com afeto e ciência. <br />
         </h1>
 
         <p>
           Apoio especializado para crianças, adolescentes e adultos que
-          enfrentam dificuldades de aprendizagem, com uma abordagem
-          integrativa e humanizada.
+          enfrentam dificuldades de aprendizagem, com uma abordagem integrativa
+          e humanizada.
         </p>
 
-
-          <button className="button-secondary">
-            Saiba Mais
-          </button>
-        
+        <button className="button-secondary">Saiba Mais</button>
       </div>
 
       <div className="hero-image">
