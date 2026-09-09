481ebf6d8e68f349e08578f0a207d929920ea246
Alexandre Akkt <alexandreskkt@gmail.com>
2026-09-09 19:54:37 -0300

    Atualizando cores do texto

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,19 @@
-// src/components/Card/Card.jsx
+import PropTypes from "prop-types";
 import "./Card.css";
 
 function Card({ title, description }) {
   return (
     <div className="card">
       <h3>{title}</h3>
+
       {description && <p>{description}</p>}
     </div>
   );
 }
+
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+};
 
 export default Card;
