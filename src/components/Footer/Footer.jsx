f9805d32caf8cf1f465278968f0c7cb34394eabf
Alexandre Henrique dos Anjos <alexandreskkt@gmail.com>
2026-09-18 16:49:59 -0300

    Melhorando a seção Foote

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,13 +3,15 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <h2>🧠 ErikaNeuroPsicoPedagoga</h2>
+      <h2>
+        <span className="footer-highlight">Erika</span> NeuroPsicoPedagoga
+      </h2>
 
       <p>Neuropsicopedagogia com afeto e ciência</p>
 
-      <span>
-        © 2026 NeuroPsico. Todos os direitos reservados.
-      </span>
+      <div className="footer-divider" />
+
+      <span>© 2026 NeuroPsico. Todos os direitos reservados.</span>
     </footer>
   );
 }
